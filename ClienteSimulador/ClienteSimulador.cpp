4b4bd781e5eea21a26a770bbba17efeb5b62680b
Javier Díaz <f15javi@gmail.com>
2025-09-27 10:13:21 +0200

    Add telemetry reader from the simulator

--- ClienteSimulador/ClienteSimulador.cpp
+++ ClienteSimulador/ClienteSimulador.cpp
@@ -1,19 +1,77 @@
-// ClienteSimulador.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <iostream>
+
+#include "../src/xplaneConnect.h"
+#ifdef WIN32
+#include <Windows.h>
+#define sleep(n) Sleep(n * 1000)
+#endif
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printf("XPlaneConnect Example: readDATA()\n\n");
+
+    // Open Socket
+    const char* xpIP = "127.0.0.1"; //IP Address of computer running X-Plane
+    unsigned short xpPort = 49007;  //default port number XPC listens on
+    unsigned short port = 49003;    //port number to which X-Plane is set to send UDP packets
+
+
+
+    while (1) {
+
+
+
+
+        XPCSocket sock = aopenUDP(xpIP, xpPort, port); //docs seem to be outdated on these...
+
+        // Read 2 rows of data
+        const int rows = 9;
+        float data[rows][9]; //data[0] is dataset index number, data[1] to data[9] are the contents of the dataset
+        readDATA(sock, data, rows);
+
+
+
+        printf("\nVtas: %f,", data[0][2]);
+
+        printf("\n pitch = %f, roll = %f, heading = %f, alt = %f", data[1][1], data[1][2], data[1][3], data[2][3]);
+
+
+        closeUDP(sock);
+
+        int speed = (int)data[0][2];
+        int altitude = (int)data[2][3];
+        int rpm = (int)data[3][1];
+        int temp = (int)data[5][1];
+        int OilP = (int)data[6][1];
+        int OilT = (int)data[7][1];
+        char telemetry1[64];
+        char telemetry2[64];
+
+        sprintf_s(telemetry1, "1 %d %f %f %f %d %d %f",
+            speed,
+            data[1][1], data[1][2], data[1][3],
+            altitude,
+            rpm,
+            data[4][1]);
+
+        sprintf_s(telemetry2, "2 %d %d %d %f %f",
+            temp,
+            OilP,
+            OilT,
+            data[8][1], data[8][2]);
+
+        Sleep(10);
+
+
+        
+
+
+    }
+
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
